028b7baf6f106a869e5f712968bb6650f7584a6d
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-12-08 10:22:55 +0200

    create table hotel

---
+++ HotelsDB/dbo/Tables/Hotel.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Hotel]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [_Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(1000) NOT NULL, 
+    [RegionId] INT NOT NULL, 
+    [RoomsId] INT NOT NULL, 
+    [RoomCount] INT NOT NULL, 
+    [FreeRoomCount] INT NOT NULL, 
+    [GuestsInHotel] INT NULL, 
+    CONSTRAINT [FK_Hotel_Region] FOREIGN KEY (RegionId) REFERENCES Region(id), 
+    CONSTRAINT [FK_Hotel_Rooms] FOREIGN KEY (RoomsId) REFERENCES Rooms(id), 
+    CONSTRAINT [FK_Hotel_Guests] FOREIGN KEY (GuestsInHotel) REFERENCES Guests(id)
+)
--- HotelsDB/dbo/Tables/Region.sql
+++ HotelsDB/dbo/Tables/Region.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Region]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Region] NVARCHAR(50) NULL, 
-    [City] NCHAR(20) NOT NULL
+    [Region] NVARCHAR(50) NOT NULL, 
+    [City] NCHAR(20) NOT NULL, 
+    [Hotels] NVARCHAR(50) NULL, 
+    CONSTRAINT [FK_Region_Hotel] FOREIGN KEY (Hotels) REFERENCES Hotel([_Name])
 )
